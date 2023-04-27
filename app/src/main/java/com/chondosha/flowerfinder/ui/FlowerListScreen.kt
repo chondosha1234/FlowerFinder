01799b9fdd722103615f9c5c09212ad2f7916eab
chondosha1234 <jmiller61193@gmail.com>
2023-04-27 01:05:58 -0400

    added implicit intent for camera, file.xml and changed manifest for camera

--- app/src/main/java/com/chondosha/flowerfinder/ui/FlowerListScreen.kt
+++ app/src/main/java/com/chondosha/flowerfinder/ui/FlowerListScreen.kt
@@ -1,12 +1,20 @@
 package com.chondosha.flowerfinder.ui
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.core.content.FileProvider
+import com.chondosha.flowerfinder.R
 import com.chondosha.flowerfinder.model.FlowerEntry
+import java.io.File
 import java.util.*
 
 @Composable
@@ -19,11 +27,12 @@ fun PhotoListScreen(
             onNavigateToDetail = onNavigateToDetail
         )
 
-        takePictureButton()
+        takePictureButton(
+            modifier = modifier
+        )
     }
 
 }
-
 
 @Composable
 fun PhotoList(
@@ -51,9 +60,30 @@ fun PhotoList(
 fun takePictureButton(
     modifier: Modifier = Modifier
 ) {
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture(),
+        onResult = { didTakePhoto ->
+            if (didTakePhoto) {
+                // use view model to create photo entry
+            }
+        }
+    )
+    val context = LocalContext.current
     Button(
-        onClick = { /*TODO*/ }
+        onClick = {
+            val photoName = "IMG_${Date()}.JPG"
+            val photoFile = File(context.applicationContext.filesDir, photoName)
+            val photoUri = FileProvider.getUriForFile(
+                context,
+                "com.chondosha.flowerfinder.fileprovider",
+                photoFile
+            )
+            launcher.launch(photoUri)
+        }
     ) {
-        Text(text = "Take Picture")
+        Image(
+            painter = painterResource(R.drawable.camera_button),
+            contentDescription = null)
     }
 }
+
